785e7ad773363d38c6f91ca54a9830b79267acfa
Renato Nandes <renato.nandes@estudante.ufjf.br>
2025-10-09 22:30:55 -0300

    :memo: Adiciona Children

--- src/Exibicao.tsx
+++ src/Exibicao.tsx
@@ -1,17 +1,20 @@
 import "./Exibicao.css"
+import type { ReactNode } from "react";
 
 type exibicaoProps = {
     inicio: Date;
     fim: Date;
     cercado: String;
+    children?: ReactNode;
 }
 
-export default function Exibicao ({inicio,fim,cercado} : exibicaoProps) {
+export default function Exibicao({ inicio, fim, cercado, children }: exibicaoProps) {
     return (
         <div className="exibicao">
             <div className="inicio">Abertura: {inicio.toLocaleString()}</div>
             <div className="fim">Encerramento: {fim.toLocaleString()}</div>
             <div className="cercado">Local: {cercado.toLocaleString()}</div>
+            <div className="animais">{children}</div>
         </div>
     )
 
